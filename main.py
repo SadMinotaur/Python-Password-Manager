5d69178603ce5e74417cc38ee9ea0d49738922aa
Dima <dimasilolil@gmail.com>
2021-02-21 20:11:51 +0600

    write find functions

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from Crypto.Cipher import AES
 
-# s = AES.new()
 
-def writeNew(name, content):
+def writeFileNew(name):
     f = open(name, "w")
-    f.write(content)
     f.close()
 
 
-def writeApp(name, content):
-    f = open(name, "a")
-    f.write(content)
+def writeFileAdd(fileName, service, login, password):
+    f = open(fileName, "a")
+    f.write(service + ";" + login + ";" + password + "\n")
     f.close()
 
 
-def readFile(name):
+def readFileAll(name):
     f = open(name, "r")
-    con = f.read()
+    con = map(lambda a: a.split(";"), f.read().splitlines())
     f.close()
     return con
+
+
+def findService(name, fileName):
+    c = readFileAll(fileName)
+    for line in c:
+        if line[0] == name:
+            return line
+    return "No such line"
+
+
+writeFileNew("test.txt")
+writeFileAdd("test.txt", "test1", "test2", "test3")
+writeFileAdd("test.txt", "test", "test2", "test3")
+writeFileAdd("test.txt", "test1", "test2", "test3")
+print(findService("test", "test.txt"))
+# s = AES.new()
