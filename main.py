2247d1b5ae6d3b52d28b47a5b3b116a1eb98b0a7
Dima <dimasilolil@gmail.com>
2021-02-24 10:33:02 +0600

    encription added

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA3_256
+from Crypto.Util.Padding import pad, unpad
+BLOCK_SIZE = 16
 
 
-def writeFileNew(name):
+def writeFileNew(name: str):
     f = open(name, "w")
     f.close()
 
 
-def writeFileAdd(fileName, service, login, password):
+def writeFileAdd(fileName: str, service: str, login: str, password: str):
     f = open(fileName, "a")
     f.write(service + ";" + login + ";" + password + "\n")
     f.close()
 
 
-def readFileAll(name):
+def readFileAll(name: str):
     f = open(name, "r")
     con = map(lambda a: a.split(";"), f.read().splitlines())
     f.close()
     return con
 
 
-def findService(name, fileName):
+def findService(name: str, fileName: str):
     c = readFileAll(fileName)
     for line in c:
         if line[0] == name:
@@ -27,9 +30,20 @@ def findService(name, fileName):
     return "No such line"
 
 
-writeFileNew("test.txt")
-writeFileAdd("test.txt", "test1", "test2", "test3")
-writeFileAdd("test.txt", "test", "test2", "test3")
-writeFileAdd("test.txt", "test1", "test2", "test3")
-print(findService("test", "test.txt"))
-# s = AES.new()
+sha = SHA3_256.new()
+sha.update(b"test")
+key = sha.hexdigest()[:BLOCK_SIZE].encode('utf_8')
+
+cipher = AES.new(key, AES.MODE_ECB)
+msg = cipher.encrypt(pad(b'test', BLOCK_SIZE))
+print(msg.hex())
+
+decipher = AES.new(key, AES.MODE_ECB)
+msg_dec = decipher.decrypt(msg)
+print(unpad(msg_dec, BLOCK_SIZE))
+
+# writeFileNew("test.txt")
+# writeFileAdd("test.txt", "test1", "test2", "test3")
+# writeFileAdd("test.txt", "test", "test2", "test3")
+# writeFileAdd("test.txt", "test1", "test2", "test3")
+# print(findService("test", "test.txt"))
